3720f422496ce0756fecb667b801213716aa71ef
Diego Cruz <diego.cruz02.bp@gmail.com>
2025-10-22 10:43:17 -0600

    Update USART.c
    Se agrega y configura el PC13 para colocar un botón y también funciones para enviar y leer datos, además de que dentro del bucle principal se define un envió y receptor dependiendo del caso

--- USART.c
+++ USART.c
@@ -7,6 +7,7 @@
 #define RCC_BASE 0x40021000
 #define GPIOA_BASE 0x40010800
 #define GPIOB_BASE 0x40010C00
+#define GPIOC_BASE 0x40011000
 #define USART1_BASE 0x40013800
 
 //Definimos la base del registro del reloj de los pines (ya que esta en bajo consumo para los arm)
@@ -15,13 +16,16 @@
 
 #define GPIOA_CRL (*(volatile uint32_t*)(GPIOA_BASE+0x00))
 #define GPIOA_CRH (*(volatile uint32_t*)(RCC_BASE+0x04))
-#define GPIOA_ODR (*((volatile uint32_t*)(RCC_BASE+0x0C))
+#define GPIOA_ODR (*(volatile uint32_t*)(RCC_BASE+0x0C))
 
 
 #define GPIOB_CRL (*(volatile uint32_t*)(GPIOB_BASE+0x00))
 #define GPIOB_ODR (*(volatile uint32_t*)(GPIOB_BASE + 0x0C))
+	
+#define GPIOC_CRH (*(volatile uint32_t*)(GPIOC_BASE +0x04))
+#define GPIOC_IDR (*(volatile uint32_t*)(GPIOC_BASE + 0x08))
 
-#define GPIOC
+#define GPIOC 
 #define USART1_SR (*(volatile uint32_t*)(USART1_BASE +0x00))
 #define USART1_DR (*(volatile uint32_t*)(USART1_BASE + 0x04))
 #define USART1_BRR (*(volatile uint32_t*)(USART1_BASE +0x08))
@@ -46,6 +50,10 @@ int main (void){
     GPIOB_CRL &= ~(0xF << (0 * 4));
     GPIOB_CRL |=  (0x2 << (0 * 4));
 
+	//PC13 para conectar el boton como entrada flotante
+	
+	 GPIOC_CRH &= ~(0xF << ((13-8)*4));
+   GPIOC_CRH |=  (0x4 << ((13-8)*4));
 	
 	//Vamos a configurar el USART1
 	//
@@ -54,20 +62,35 @@ int main (void){
 	
 	USART1_CR1 |= 0x2000;
 	
-	//Vamos a definir las funcionas para enviar, recibir y leer datos con USART
 	
-	//El receptor va dentro del while
+	//Dentro del while detectaremos si nos llega una señal o nosotros enviamos una 
 
-}
-//Vamos a definir las funcionas para enviar, recibir y leer datos con USART
 	while(1){
 	//Al presionar un boton queremos encender el LED
+		//si el bit 13 de GPIOC_IDR es igual a 0 quiere decir que el boton se apreto
+		if (!(GPIOC_IDR & (1 << 13))) 
+			{ 
+        GPIOA_ODR |= (1 << 8);  // Enciende LED local
+        transmitir('1');  // Envía señal al otro micro
+    }
+
+	//Al recibir un un dato se enciende un led 
+		
+	if(USART1_SR &(1 << 5) ){
 		
 		
-	//Al recibir un boton se enciende un led 
+		char dato = USART1_DR;
+		if(dato=='1'){
+			GPIOB_ODR ^= (1<<0);
+		
+		}
 	
 	}
+	
+}
 
+}
+	//Vamos a definir las funcionas para enviar y leer datos con USART
 void transmitir(char c)
 		{
 		while (!(USART1_SR & (1 << 7)));
